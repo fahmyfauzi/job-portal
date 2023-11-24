ce22ad167ab5d0e0a4480d0d6f262518be945c07
fahmyfauzi <fahmyfauzii@gmail.com>
2023-11-24 23:42:14 +0700

    pagination

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -61,12 +61,24 @@ const getJobsControoler = async (req, res, next) => {
   if (sort == "z-a") {
     queryResult = queryResult.sort("-position");
   }
+
+  //pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  queryResult = queryResult.skip(skip).limit(limit);
+  //jobs count
+  const totalJobs = await Job.countDocuments(queryResult);
+  const numOfPage = Math.ceil(totalJobs / limit);
+
   // Eksekusi query dan simpan hasilnya dalam variabel jobs
   const jobs = await queryResult;
 
   //response
   res.status(200).json({
-    totalJobs: jobs.length,
+    totalJobs,
+    page,
     jobs,
   });
 };
